34907c7e8e72000c21774f4805a2a5a48c8fca13
TheIzvorg <TheIzvorg@DESKTOP-4IKOL7T>
2021-09-22 13:43:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/messenger/UserActivity.java
@@ -0,0 +1,18 @@
+package com.example.messenger;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
